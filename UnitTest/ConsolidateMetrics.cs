d72a61020bb51877a129ccd9f582b74b74a0a75c
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2017-12-12 20:56:11 -0200

    Added task and project to session object.

--- UnitTest/ConsolidateMetrics.cs
+++ UnitTest/ConsolidateMetrics.cs
@@ -58,6 +58,21 @@ namespace UnitTest
         //
         #endregion
 
+        public class TaskData
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Action { get; set; }
+            public DateTime Created { get; set; }
+            public ProjectData Project { get; set; }
+        }
+
+        public class ProjectData
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+
         public class PathNodeParameterData
         {
             public string Type { get; set; }
@@ -85,6 +100,7 @@ namespace UnitTest
             public DateTime Started { get; set; }
             public DateTime Finished { get; set; }
             public List<PathNodeData> PathNodes { get; set; } = new List<PathNodeData>();
+            public TaskData Task { get; set; }
         }
 
         public class CodeMetric
@@ -102,6 +118,21 @@ namespace UnitTest
         [TestMethod]
         public void TestMethod1()
         {
+            //-------- Read code metrics
+
+            string fileCodeMetrics = @"C:\Users\EduardoAFontana\Downloads\Coleta\Consolidado-2\CodeMetrics\FastCodeMetrics.txt";
+
+            string objJsonDataCodeMetrics = String.Empty;
+
+            using (StreamReader file = new StreamReader(fileCodeMetrics, Encoding.UTF8))
+            {
+                objJsonDataCodeMetrics = file.ReadToEnd();
+            }
+
+            List<CodeMetric> codeMetrics = Newtonsoft.Json.JsonConvert.DeserializeObject<List<CodeMetric>>(objJsonDataCodeMetrics);
+
+            //--------- Read Sessions files
+
             string[] fileEntries = Directory.GetFiles(@"C:\Users\EduardoAFontana\Downloads\Coleta\Consolidado-2");
 
             foreach (string fileName in fileEntries)
@@ -127,20 +158,6 @@ namespace UnitTest
                 Sessions.Add(session);
             }
 
-            //-------- Read code metrics
-
-            string fileCodeMetrics = @"C:\Users\EduardoAFontana\Downloads\Coleta\Consolidado-2\CodeMetrics\FastCodeMetrics.txt";
-
-            string objJsonDataCodeMetrics = String.Empty;
-
-            using (StreamReader file = new StreamReader(fileCodeMetrics, Encoding.UTF8))
-            {
-                objJsonDataCodeMetrics = file.ReadToEnd();
-            }
-
-            List<CodeMetric> codeMetrics = Newtonsoft.Json.JsonConvert.DeserializeObject<List<CodeMetric>>(objJsonDataCodeMetrics);
-
-            //--------- 
 
             //foreach (Task task in Tasks)
             //{
