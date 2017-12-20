2e96558996f1f9dc98b53d3c4d39444327242fbd
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2017-12-20 18:47:27 -0200

    Removed close window button from session input form.

--- SwarmClientVS/SessionInputForm.Designer.cs
+++ SwarmClientVS/SessionInputForm.Designer.cs
@@ -125,6 +125,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 386);
+            this.ControlBox = false;
             this.Controls.Add(this.lblProject);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.gbTask);
